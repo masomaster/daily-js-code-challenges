c3e7f999335dedb327b6b47ba9ed330d43e93cae
masomaster <masonlancaster@protonmail.com>
2022-09-07 13:15:04 -0700

    updated d2_notes

--- challenges.js
+++ challenges.js
@@ -60,9 +60,10 @@ addTwoNumbers(0, 0) //=> 0
 addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
-
-
-
+function addTwoNumbers(num1, num2) {
+  if (typeof num1 === "number" && typeof num2 === "number") return num1+num2;
+  else return "NaN";
+}
 
 
 
